287c11fbcf08ef36cfae26e9f510786fc5218ead
Developer <Developer@Johns-MacBook-Air-2.local>
2023-01-03 13:37:25 -0500

    Stuff.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'};
+
 function App() {
-  const title = 'React';
+  
 
 
   return (
     <div>
-      <h1>Hello {title}.</h1>
+      <h1>{welcome.greeting}, {welcome.title}.</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
